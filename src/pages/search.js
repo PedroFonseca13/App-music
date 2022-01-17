99ab808e4de7716a1945eb7763968d640c368e9c
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-01-17 15:19:05 -0300

    requirement 05 - done

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { MIN_LENGTH_INPUT_SEARCH } from '../CONST';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { searchInput } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <form>
+          <label htmlFor="searchInput">
+            <input
+              type="text"
+              name="searchInput"
+              id="searchInput"
+              value={ searchInput }
+              onChange={ this.handleChange }
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchInput.length < MIN_LENGTH_INPUT_SEARCH }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
