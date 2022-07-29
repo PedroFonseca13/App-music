f4b5e6d0464d8982f8fe39ac07ac35b680c5e0e5
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-29 16:24:06 -0300

    style: adicionado novo estilo a pagina de login
    Alteracoes feitas na pagina de login da aplicacao. Adicionado também a lib bootstrap

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import { Redirect } from 'react-router-dom';
-import { MIN_LENGTH_INPUT_FORM } from '../CONST';
 import { createUser } from '../services/userAPI';
+import { MIN_LENGTH_INPUT_FORM } from '../CONST';
 
 import Loading from '../components/Loading';
 import '../style/Login.css';
@@ -36,32 +40,43 @@ export default class Login extends Component {
     }
 
     return (
-      <main data-testid="page-login" className="page-login-container container">
-        <form className="form-login">
-          <label htmlFor="inputName">
-            <h2>Login</h2>
-            <input
-              type="text"
-              id="inputName"
-              name="inputName"
-              value={ inputName }
-              onChange={ this.handleChange }
-              data-testid="login-name-input"
-              placeholder="Nome"
-              className="inputName-container"
-            />
-          </label>
-          <button
-            type="button"
-            disabled={ inputName.length < MIN_LENGTH_INPUT_FORM }
-            onClick={ this.validateLogin }
-            className="button-container"
-            data-testid="login-submit-button"
+      <main data-testid="page-login" id="main">
+        <div
+          className="container d-flex justify-content-center align-items-center box-form"
+        >
+          <Form
+            className="form px-2 py-4 rounded align-items-center border border-success"
           >
-            Entrar
-          </button>
-          { isRedirect && <Redirect to="/search" /> }
-        </form>
+            <h2 className="title text-light text-center">App de musicas</h2>
+            <Form.Text className="text-light mb-2">
+              Ouça previas de suas musicas favoritas dentro do app de musicas.
+            </Form.Text>
+            <Form.Group className="" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                placeholder="Insert your name"
+                name="inputName"
+                value={ inputName }
+                onChange={ this.handleChange }
+                data-testid="login-name-input"
+                className="inputName-container"
+              />
+            </Form.Group>
+            <Button
+              variant="success"
+              type="submit"
+              disabled={ inputName.length < MIN_LENGTH_INPUT_FORM }
+              onClick={ this.validateLogin }
+              className="mt-3 mx-auto d-flex button"
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </Button>
+
+            { isRedirect && <Redirect to="/search" /> }
+          </Form>
+        </div>
+
       </main>
     );
   }
